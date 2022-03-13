faad6a5cb3661e37b963960e4732f9dd50cd1102
iskyldrm <m.isakyildirim@gmail.com>
2022-03-13 20:08:35 +0300

    Udemy için repository oluşturuldu ve ilk proje gönderildi

--- Udemy-ConsoleApp1/Program.cs
+++ Udemy-ConsoleApp1/Program.cs
@@ -10,6 +10,8 @@ namespace Udemy_ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
         }
     }
 }
