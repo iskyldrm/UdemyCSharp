5137e595e96c30e334003b3ca034d4930a125411
iskyldrm <isakyildirim@outlook.com>
2022-03-18 14:45:44 +0300

    denemme

--- Udemy-ConsoleApp1/Program.cs
+++ Udemy-ConsoleApp1/Program.cs
@@ -10,6 +10,8 @@ namespace Udemy_ConsoleApp1
     {
         static void Main(string[] args)
         {
+
+            var burbirnesne = new Program();            
             Console.WriteLine("Hello World");
             Console.ReadLine();
         }
