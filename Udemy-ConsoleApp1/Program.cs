af3d2559016ad8a8843cd0964b886e8e841e71e0
iskyldrm <isakyildirim@outlook.com>
2022-03-18 16:05:33 +0300

    thread denendi

--- Udemy-ConsoleApp1/Program.cs
+++ Udemy-ConsoleApp1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Udemy_ConsoleApp1
@@ -11,7 +12,11 @@ namespace Udemy_ConsoleApp1
         static void Main(string[] args)
         {
 
-            var burbirnesne = new Program();            
+            var burbirnesne = new Program();
+            Console.WriteLine("slkdmsaldmas");
+
+
+            Thread.Sleep(5000);
             Console.WriteLine("Hello World");
             Console.ReadLine();
         }
