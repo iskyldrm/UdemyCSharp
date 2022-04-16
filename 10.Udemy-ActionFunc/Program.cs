03ddbc7b8ece0c2a7883f157759581150e71aefd
isak yıldırım <isakyildirim@outlook.com>
2022-04-16 16:53:13 +0300

    delegates eventlar

---
+++ 10.Udemy-ActionFunc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _10.Udemy_ActionFunc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 9.Udemy-DelegateEvent/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _9.Udemy_DelegateEvent
+{
+    delegate int del(int x);
+    delegate string del2(string x);
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Click += Form1_Click;
+
+            del goster = x => x * x;
+            int y = goster(3);
+            MessageBox.Show(y.ToString());
+
+            Button button = new Button();
+
+            button.Text = "tıkla";
+            button.Name = "btntikla";
+            button.Width = 100;
+            button.Height = 50;
+            button.Left = 200;
+            button.Top = 200;
+            button.Click += delegate (object sender, EventArgs e)
+            {
+                string mesaj = (sender as Button).Text;
+                MessageBox.Show(mesaj);
+            };
+            this.Controls.Add(button);
+
+        }
+
+        private void Form1_Click(object? sender, EventArgs e)
+        {
+            MessageBox.Show(((MouseEventArgs)e).Location.ToString());
+        }
+    }
+}
