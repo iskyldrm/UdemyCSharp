3547ccb15de37d11c3fb636a2d6d5b8659f58254
isak yıldırım <isakyildirim@outlook.com>
2022-04-16 17:16:03 +0300

    action calisildi

--- 10.Udemy-ActionFunc/Program.cs
+++ 10.Udemy-ActionFunc/Program.cs
@@ -7,6 +7,16 @@ namespace _10.Udemy_ActionFunc
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Action<int, string> action = new Action<int, string>(KullaniciBilgileri);
+            action.Invoke(3,"İsak Yıldırım");
+        }
+
+
+        static void KullaniciBilgileri(int KulId, string KulAdi)
+        {
+            Console.WriteLine("Kullanıcı Id:  " + KulId);
+            Console.WriteLine("Kullanıcı Adi: " + KulAdi);
         }
     }
+
 }
